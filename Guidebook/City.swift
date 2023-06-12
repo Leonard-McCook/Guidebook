35dac4b7dd609d089dd8fb8dc9f295a782c0695d
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-06-12 17:15:40 -0400

    Create DataService file and City file

---
+++ Guidebook/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  Guidebook
+//
+//  Created by Leonard McCook-Carr on 6/12/23.
+//
+
+import Foundation
+
+struct City {
+    
+    
+    
+}
---
+++ Guidebook/DataService.swift
@@ -0,0 +1,14 @@
+//
+//  DataService.swift
+//  Guidebook
+//
+//  Created by Leonard McCook-Carr on 6/12/23.
+//
+
+import Foundation
+
+struct DataService {
+    
+    
+    
+}
