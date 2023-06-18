b50a0f88dc5163907f6ca57249d2d4e35ebb0067
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-06-18 19:24:40 -0400

    Create CityCard file

---
+++ Guidebook/CityCard.swift
@@ -0,0 +1,20 @@
+//
+//  CityCard.swift
+//  Guidebook
+//
+//  Created by Leonard McCook-Carr on 6/18/23.
+//
+
+import SwiftUI
+
+struct CityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CityCard()
+    }
+}
